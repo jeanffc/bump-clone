a12f1146bf3d10baa67aee8f79c55c3e1c0d4d9c
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2022-06-08 19:54:56 -0400

    feat(): created html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'midnight': "rgba(21,34,83,var(--tw-bg-opacity))",
+      },
+    },
   },
   plugins: [],
 }
