7b8800e3c06a6d3309c7dc2f1a74325ff28dd6cc
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2022-06-08 20:35:07 -0400

    fix(style): created custom fontSize

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
       colors: {
         'midnight': "rgba(21,34,83,var(--tw-bg-opacity))",
       },
+      fontSize: {
+        'base': '0.95rem',
+        '6xl': '3.60rem',
+      }
     },
   },
   plugins: [],
